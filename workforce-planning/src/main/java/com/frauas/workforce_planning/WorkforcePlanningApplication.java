8d07db641148dc58d87b325b45522a0c72a585ae
Avradip Mazumdar <rontymazumdar@gmail.com>
2025-11-23 18:21:51 +0100

    Creation of Maven project with Springboot Application

---
+++ workforce-planning/src/main/java/com/frauas/workforce_planning/WorkforcePlanningApplication.java
@@ -0,0 +1,13 @@
+package com.frauas.workforce_planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforcePlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforcePlanningApplication.class, args);
+	}
+
+}
---
+++ workforce-planning/src/test/java/com/frauas/workforce_planning/WorkforcePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frauas.workforce_planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforcePlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
