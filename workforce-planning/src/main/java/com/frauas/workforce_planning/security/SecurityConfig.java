fa8e23d0be2fcf19caa2a7d44d0bcaf7fceeec47
Avradip Mazumdar <rontymazumdar@gmail.com>
2026-01-28 02:39:28 +0100

    Fixing errors

--- workforce-planning/src/main/java/com/frauas/workforce_planning/security/SecurityConfig.java
+++ workforce-planning/src/main/java/com/frauas/workforce_planning/security/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
                             "/v3/api-docs/**",
                             "/v3/api-docs.yaml",
                             "/swagger-resources/**",
-                            "/webjars/**"
+                            "/webjars/**",
+                            "/error"
                         ).permitAll()
                         
                         // Public: Team 3B Integration endpoints
